c20609ecbdad5630ef903bc7d4a10de87c3e437d
Viren Bhagat <virenb@gmail.com>
2019-05-15 15:00:43 -0400

    Building out basic form, hooks, and set up linting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
+
+/*
+API
+History - https://api.openrates.io/2000-01-03?base=USD
+Latest - https://api.openrates.io/latest?base=USD
+*/
 
 function App() {
+  const [date, setDate] = useState(null);
+
   return (
     <div>
-      Hello World
+      <header>
+        <h1>ForEx Tracker</h1>
+      </header>
+      <main>
+        <section>
+          <h3>USD <span role="img" aria-label="img">🇺🇸</span></h3>
+          <p>Date: {date}</p>
+          <form onSubmit={e => e.preventDefault()}>
+            <button
+              type="button"
+              value={new Date().toISOString().split('T')[0]}
+              onClick={e => setDate(e.target.value)}
+            >
+              Current
+            </button>
+            <input
+              type="date" id="history-date" min="1999-01-04" max="2018-12-31"
+              onChange={e => setDate(e.target.value)}
+            />
+            <button type="submit" onClick={() => console.log(`${date}`)}>Submit</button>
+          </form>
+        </section>
+      </main>
     </div>
   );
 }
